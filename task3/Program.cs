9e9129692f7092f179a1b4f368f841037fa51a09
unknown <hurina.irina@mail.ru>
2022-10-11 01:56:32 +0300

    task3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет */
+
+void GetNum (int number)
+{
+    if (number>=1 && number <=5)
+    {
+        Console.WriteLine($"{number} -> no");
+        return;
+    }
+
+    if (number>=6 && number <=7)
+    {
+        Console.WriteLine($"{number} -> yes");
+        return;
+    }
+
+    else
+    {
+    Console.WriteLine("Введено неверное число");
+    }
+}
+
+GetNum (-3);
+
+
+
+
+
+
+
+
