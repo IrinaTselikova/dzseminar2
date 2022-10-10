ccce6d4add0e2d38b3d94d41f9270a0aeda832a7
unknown <hurina.irina@mail.ru>
2022-10-11 00:09:17 +0300

    result - не существует в текущем контексте

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+int GetNumber (int min, int max)
+{
+    Random rnd = new Random();
+    int result = rnd.Next(min, max);
+    return result;
+}
+
+int GetSecondNumber (int result)
+{
+   string numericAsString = result.ToString();
+   int resultString = int.Parse(numericAsString[2].ToString());
+   return resultString;
+}
+
+int number = GetNumber(100, 1000);
+int number2 = GetSecondNumber (resultString);
+Console.WriteLine($"{number} ->{number2}");
