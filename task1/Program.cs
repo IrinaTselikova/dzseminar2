7891635f341964e8a9b2b90ed788ff6f0942633f
unknown <hurina.irina@mail.ru>
2022-10-11 09:33:50 +0300

    task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -3,20 +3,12 @@
 782 -> 8
 918 -> 1 */
 
-int GetNumber (int min, int max)
+void GetSecondNumber ()
 {
-    Random rnd = new Random();
-    int result = rnd.Next(min, max);
-    return result;
+    int newNumber = (new Random()).Next(100, 1000);
+    string numberStr = newNumber.ToString();
+    int result = int.Parse(numberStr[1].ToString());
+    Console.WriteLine($"{newNumber} -> {result}");
 }
 
-int GetSecondNumber (int result)
-{
-   string numericAsString = result.ToString();
-   int resultString = int.Parse(numericAsString[2].ToString());
-   return resultString;
-}
-
-int number = GetNumber(100, 1000);
-int number2 = GetSecondNumber (resultString);
-Console.WriteLine($"{number} ->{number2}");
+GetSecondNumber();
